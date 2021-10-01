19182a2773ed9d06da93a1783693669e225b7022
Christopher Eyre <christopher.eyre@codurance.com>
2021-10-01 15:36:50 +0100

    Added a stacked modal dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbdModalStacked } from './modal-stacked/modal-stacked.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdModalStacked
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
